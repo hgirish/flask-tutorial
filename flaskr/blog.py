5adfa2fbee15c6523154461b69cad041283551ed
hgirish <girish@post.com>
2020-12-31 16:13:28 -0800

    init_db

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -4,13 +4,15 @@ from flask import (
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
-from flaskr.db import get_db
+from flaskr.db import get_db,init_db
 
 bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
     db = get_db()
+    init_db()
+
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
