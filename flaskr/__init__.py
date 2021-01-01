f741a001b397be80f04f414a809a047bd4450baa
hgirish <girish@post.com>
2020-12-31 17:20:17 -0800

    config py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -13,7 +13,8 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('config.py', silent=False)
+        app.config.from_pyfile('ProdConfig.py', silent=True)
     else:
         # load the test config if passed on
         app.config.from_mapping(test_config)
--- flaskr/config.py
+++ flaskr/config.py
@@ -4,15 +4,9 @@ from os import environ, path
 
 
 basedir = path.abspath(path.dirname(__file__))
-
-
-
-class Config:
-    """Set Flask config variables."""
-
-
-    SECRET_KEY = environ.get('SECRET_KEY')
-    DATABASE = environ.get("DATABASE")
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
+"""Set Flask config variables."""
+SECRET_KEY = environ.get('SECRET_KEY')
+DATABASE = '/home/site/db/flakergo.sqlite'
+STATIC_FOLDER = 'static'
+TEMPLATES_FOLDER = 'templates'
 
