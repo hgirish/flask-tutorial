8f3b87f77fd3cb81dc843f4fb3cd4c4225343377
hgirish <girish@post.com>
2020-12-31 17:21:40 -0800

    check if secret key works

--- flaskr/test.py
+++ flaskr/test.py
@@ -12,5 +12,5 @@ bp = Blueprint('test', __name__)
 @bp.route("/test")
 def test():
   database = environ.get('DATABASE')
-  database = current_app.config['DATABASE']
+  database = current_app.config['SECRET_KEY']
   return "DATABASE is at : %s" % (database, )
